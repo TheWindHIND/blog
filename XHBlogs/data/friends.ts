f2a78a588dd297db2912605d04d5a38560b3c67d
TheWindHIND <2927015997@qq.com>
2026-06-20 20:00:48 +0800

    更新：周六 2-26-06 20:00

--- XHBlogs/data/friends.ts
+++ XHBlogs/data/friends.ts
@@ -1,4 +1,13 @@
 // 🛡️ 本文件由 XingHuiSama 控制台自动生成
 export interface Friend { id: string; name: string; url: string; description: string; avatar: string; themeColor: string; }
 
-export const friendsData: Friend[] = [];
+export const friendsData: Friend[] = [
+  {
+    "id": "friend_1781956200205",
+    "name": "烟柳",
+    "url": "https://www.cnblogs.com/liu-qingyan",
+    "avatar": "https://backup.fukit.cn/autoupload/f/e6dvVv6sI6US7bqgWvSMp9iO_OyvX7mIgxFBfDMDErs/20260620/6tch/240X240/mRsYLMNA.jpg",
+    "description": "朋友的博客",
+    "themeColor": "#f75959"
+  }
+];
