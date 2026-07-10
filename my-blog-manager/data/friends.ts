5f1d4ec6432c466d407a0a0a41daf03796e93208
TheWindHIND <2927015997@qq.com>
2026-07-10 22:14:15 +0800

    更新：~0,4datetime:~4,2datetime:~6,2datetime:~8,2datetime:~10,2

--- my-blog-manager/data/friends.ts
+++ my-blog-manager/data/friends.ts
@@ -1,4 +1,13 @@
 // 🛡️ 本文件由 XingHuiSama 控制台自动生成
 export interface Friend { id: string; name: string; url: string; description: string; avatar: string; themeColor: string; }
 
-export const friendsData: Friend[] = [];
+export const friendsData: Friend[] = [
+  {
+    "id": "friend_1783692325972",
+    "name": "柳卿烟",
+    "url": "https://liu-qinyan.github.io/qingyan-blog/",
+    "avatar": "https://liu-qinyan.github.io/qingyan-blog/assets/avatar.png",
+    "description": "这位朋友很神秘，什么都没写。",
+    "themeColor": "#64f2e8"
+  }
+];
