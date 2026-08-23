6cedd04474f92e695240ae8c0a201c4fbf710015
TheWindHIND <2927015997@qq.com>
2026-08-23 21:19:13 +0800

    更新：周日 2-26-08 21:19

--- my-blog-manager/data/projects.ts
+++ my-blog-manager/data/projects.ts
@@ -9,4 +9,15 @@ export type Project = {
   tags: string[];
 };
 
-export const projectsData: Project[] = [];
+export const projectsData: Project[] = [
+  {
+    "id": "proj_1787489465751",
+    "name": "我马上来",
+    "githubUrl": "https://thewindhind.github.io/im-coming/",
+    "description": "打开页面自动获取访问者的城市位置，显示 bro,你在XX省XX市对吧，我马上来，伴随约 1 秒的科幻故障（glitch）入场闪动特效。纯静态页面，部署于 GitHub Pages。",
+    "icon": "🚀",
+    "tags": [
+      "纯 HTML，CSS，原生 JS"
+    ]
+  }
+];
